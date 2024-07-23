158ffdf62c62adc79be85482e1df7125a33d1e39
Khayam Ijaz <ijazkhayam@gmail.com>
2024-07-23 12:31:24 +0500

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,25 +20,25 @@ const Navbar = ({navbarFunction}) => {
   
   return (
     <>
-      <div className="md:w-[85%] w-[95%] overflow-x-hidden  bg-[#ffffff59] md:z-30 z-30 fixed left-[2.5%] md:left-[7%] backdrop:blur-lg shadow-md rounded-md mx-auto h-auto py-4 md:py-5 px-8 flex items-center justify-between">
+      <div className="md:w-[85%] w-[95%] overflow-x-hidden dark:text-white  bg-[#ffffff59] md:z-30 z-30 fixed left-[2.5%] md:left-[7%] backdrop:blur-lg shadow-md rounded-md mx-auto h-auto py-4 md:py-5 px-8 flex items-center justify-between">
         <div className="logo">
-          <h1 className="text-black text-xl font-semibold">Khayam.</h1>
+          <h1 className="text-black dark:text-white text-xl font-semibold">Khayam.</h1>
         </div>
         <div className="right md:flex md:items-center md:gap-10">
           <ul className="md:flex md:items-center sm:hidden hidden gap-6">
-            <li className="text-lg cursor-pointer hover:text-black text-gray-400 transition-all duration-200">
+            <li className="text-lg cursor-pointer hover:text-black dark:hover:text-white text-gray-400 transition-all duration-200">
               Home
             </li>
-            <li className="text-lg cursor-pointer hover:text-black text-gray-400 transition-all duration-200">
+            <li className="text-lg cursor-pointer hover:text-black dark:hover:text-white text-gray-400 transition-all duration-200">
               About
             </li>
-            <li className="text-lg cursor-pointer hover:text-black text-gray-400 transition-all duration-200">
+            <li className="text-lg cursor-pointer hover:text-black dark:hover:text-white text-gray-400 transition-all duration-200">
               Skills
             </li>
-            <li className="text-lg cursor-pointer hover:text-black text-gray-400 transition-all duration-200">
+            <li className="text-lg cursor-pointer hover:text-black dark:hover:text-white text-gray-400 transition-all duration-200">
               Projects
             </li>
-            <li className="text-lg cursor-pointer hover:text-black text-gray-400 transition-all duration-200">
+            <li className="text-lg cursor-pointer hover:text-black dark:hover:text-white text-gray-400 transition-all duration-200">
               Contact
             </li>
           </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,82 +1,102 @@
-import React, { useContext, useState } from 'react'
-import Navbar from '../components/Navbar'
-import Loader from '../components/Loader'
-import Hero from '../components/Hero'
-import Full from '../components/Full'
+import React, { useContext, useState } from "react";
+import Navbar from "../components/Navbar";
+import Loader from "../components/Loader";
+import Hero from "../components/Hero";
+import Full from "../components/Full";
 import { IoIosCloseCircle } from "react-icons/io";
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ToggleButton from "react-toggle-button";
 
 
 const Home = () => {
   const [myTimeLine, setMyTimeLine] = useState();
+  const [isToggled, setIsToggled] = useState(false);
 
-  useGSAP(()=>{
-    const tl1 = gsap.timeline();
-    tl1.to(".full",{
-      right: 0,
-      duration: .5,
-    });
+  const darkMode = document.querySelector('html');
 
-    tl1.from(".liParent li",{
-      x:30,
-      opacity:0,
-      duration:.4,
-      stagger:.1,
-    });
-
-    tl1.from(".myCross",{
-      opacity:0,
-      duration:.3,
-    })
+  if(isToggled){
+    darkMode.classList.remove('light');
+    darkMode.classList.add('dark');
+  }else{
+    darkMode.classList.remove('dark');
+    darkMode.classList.add('light');
     
+  }
+
+  useGSAP(() => {
+    const tl1 = gsap.timeline();
+    tl1.to(".full", {
+      right: 0,
+      duration: 0.5,
+      borderRadius: 0,
+    });
+
+    tl1.from(".liParent li", {
+      x: 30,
+      opacity: 0,
+      duration: 0.4,
+      stagger: 0.1,
+    });
+
+    tl1.from(".myCross", {
+      opacity: 0,
+      duration: 0.3,
+    });
+
     tl1.pause();
     setMyTimeLine(tl1);
-  })
+  });
 
-  const {contextSafe} = useGSAP();
+  const { contextSafe } = useGSAP();
 
-  const handleNavClick = contextSafe(()=>{
+  const handleNavClick = contextSafe(() => {
     myTimeLine.play();
   });
 
-  const handleCross = contextSafe(()=>{
+  const handleCross = contextSafe(() => {
     myTimeLine.reverse();
-  })
-  
-  
-  
-  
-  
+  });
+
   return (
     <>
       <div className="home w-full h-screen relative md:pt-12 pt-8 overflow-x-hidden dark:bg-black bg-[#dadada]">
         <Loader />
         <Navbar navbarFunction={handleNavClick} />
 
-
-        <div className="w-full full h-full right-[-100%] md:hidden absolute top-0 bg-black z-40">
+        <div className="w-full full h-full right-[-100%] md:hidden absolute dark:bg-white top-0 bg-black z-40">
           <IoIosCloseCircle
-          onClick={handleCross}
-            className="text-white myCross absolute top-[5%] right-[5%]"
+            onClick={handleCross}
+            className="text-white dark:text-black myCross absolute top-[5%] right-[5%]"
             size={30}
           />
 
           <ul className="liParent mt-10 w-full py-20 h-[80%] p-5 flex flex-col gap-6">
-            <li className="text-white text-2xl border-b py-2 border-white">
+            <li className="text-white dark:text-black text-2xl border-b py-2 border-white dark:border-black">
               Home
             </li>
-            <li className="text-white text-2xl border-b py-2 border-white">
+            <li className="text-white dark:text-black text-2xl border-b py-2 border-white dark:border-black">
               About
             </li>
-            <li className="text-white text-2xl border-b py-2 border-white">
+            <li className="text-white dark:text-black text-2xl border-b py-2 border-white dark:border-black">
               Skills
             </li>
-            <li className="text-white text-2xl border-b py-2 border-white">
+            <li className="text-white dark:text-black text-2xl border-b py-2 border-white dark:border-black">
               Projects
             </li>
-            <li className="text-white text-2xl border-b py-2 border-white">
+            <li className="text-white dark:text-black text-2xl border-b py-2 border-white dark:border-black">
               Contact
+            </li>
+            <li className="text-white items-center dark:text-black flex justify-between text-2xl border-b py-2 border-white">
+              <h1>Dark Mode</h1>
+              <div className="toggle-btn">
+                <ToggleButton
+                  value={isToggled}
+                  onToggle={() => {
+                    setIsToggled((prev) => !prev)
+                  }}
+                />
+              </div>
             </li>
           </ul>
         </div>
@@ -84,6 +104,6 @@ const Home = () => {
       </div>
     </>
   );
-}
+};
 
-export default Home
+export default Home;
